7863fda874853f5cbf0d34c12b2a82f507513418
Uday Pradhan <87074196+upradhan93@users.noreply.github.com>
2022-02-23 16:03:43 +0530

    Update Set-License-Server.ps1

--- Set-License-Server.ps1
+++ Set-License-Server.ps1
@@ -1,5 +1,5 @@
 param($RLMServer)
 
 if ($RLMServer.Length -ne 0){ 
-New-ItemProperty -Path "Microsoft.PowerShell.Core\Registry::\HKEY_USERS\S-1-5-18\Software\GSettings\com\nicesoftware\dcv\license" -Name 'license-file' -PropertyType String -Value "5053@$RLMServer" -Force
+New-ItemProperty -Path "Microsoft.PowerShell.Core\Registry::\HKEY_USERS\S-1-5-18\Software\GSettings\com\nicesoftware\dcv\license" -Name 'license-file' -PropertyType String -Value "$RLMServer" -Force
 }
